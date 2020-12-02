88487a6540ad1e564cbb686f91d118c7cfa0f0ec
Gabe <gabesaba@gmail.com>
2020-12-01 21:43:48 -0500

    Optimize Day 1 challenge 2
    Before:
      0.273829 seconds (536.68 k allocations: 26.063 MiB)
      0.003678 seconds (47.66 k allocations: 837.594 KiB)
      0.004771 seconds (47.66 k allocations: 837.594 KiB)
    
    After:
      0.197144 seconds (421.59 k allocations: 21.961 MiB)
      0.000076 seconds (2 allocations: 16.812 KiB)
      0.000170 seconds (2 allocations: 16.812 KiB)

--- day1_1.jl
+++ day1_1.jl
@@ -1,20 +1,21 @@
 lines = readlines(stdin)
 nums = map((x) -> parse(Int32, x), lines)
-twos = Dict()
+twos = Array{Tuple{Int32, Int32}}(undef, 2020)
+fill!(twos, (0, 0))
 
 for (i, x1) in enumerate(nums)
   for x2 in view(nums, i+1:length(nums))
-    twos[x1 + x2] = (x1, x2)
+    if (x1 + x2) < 2020
+      twos[x1 + x2] = (x1, x2)
+    end
   end
 end
 
 for x3 in nums
   rem = 2020 - x3
-  if haskey(twos, rem)
-    (x1, x2) = twos[rem]
-    if x1 != x3 && x2 != x3
-      println(x1 * x2 * x3)
-      break
-    end
+  (x1, x2) = twos[rem]
+  if (x1 + x2 + x3) == 2020
+    println(x1 * x2 * x3)
+    break
   end
 end
